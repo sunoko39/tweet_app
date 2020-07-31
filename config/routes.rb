9adfc41c9473fc7995aa4a256dedd1d78c850568
Hiroshi <snkprogram@gmail.com>
2020-07-31 15:34:23 +0900

    sample1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+
+  get "signup" => "users#new"
+  get 'users/index' => "users#index"
+  get "users/:id" => "users#show"
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+
+  get "about" => "home#about"
+  root "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+end 
